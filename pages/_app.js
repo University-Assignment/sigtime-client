1a72390da56fbebbe3cd43bdc3b1b4890914e8a6
dungbik <q1w2e30630@gmail.com>
2021-05-26 00:35:03 +0900

    props 넘어가지게 수정

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 import Head from "next/head";
 import "antd/dist/antd.css";
 
-const App = ({ Component }) => {
+const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <title>SigTime</title>
       </Head>
-      <Component />
+      <Component {...pageProps} />
     </>
   );
 };
